bd6118e0efeaa454eff70d2ac24308983a41d513
ceccy-ope <muyidececilia@gmail.com>
2023-07-04 03:29:39 +0100

    update

--- src/components/Main4.jsx
+++ src/components/Main4.jsx
@@ -3,7 +3,7 @@ import main3 from '../assets/main3.png'
 
 const Main4 = () => {
   return (
-    <div className='bg-[#FFF9F5] h-[70vh] w-[100%]'>
+    <div className='bg-[#FFF9F5 h-[70vh] w-[100%]'>
         <img src={main3} alt="" />
     </div>
   )
