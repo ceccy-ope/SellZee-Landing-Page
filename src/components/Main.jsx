7b6b9e32866eee640acbe7d115a5c6c68861b822
ceccy-ope <muyidececilia@gmail.com>
2023-07-05 03:19:21 +0100

    update

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import ImageList from '../ImageList';
-import Main2 from '../Main2';
-import Main3 from '../Main3';
-import Main4 from '../Main4';
-import Main5 from '../Main5';
-import Main6 from '../Main6';
-import Main7 from '../Main7';
+import ImageList from './ImageList';
+import Main2 from './Main2';
+import Main3 from './Main3';
+import Main4 from './Main4';
+import Main5 from './Main5';
+import Main6 from './Main6'
+import Main7 from './Main7'
 
 
 const Main = () => {
